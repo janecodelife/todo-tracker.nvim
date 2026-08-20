96f1a2c7b498863d9f2c77756250aba181d3189e
Admin <admin@admin.com>
2026-08-20 17:35:50 +0300

    Update configs and readme

--- lua/todo-tracker/init.lua
+++ lua/todo-tracker/init.lua
@@ -6,13 +6,6 @@ function M.setup(opts)
 	config.setup(opts)
 
 	vim.api.nvim_create_user_command("TodoTrackerAdd", ui.add_comment, {})
-
-	vim.api.nvim_create_autocmd({ "FileType", "BufEnter" }, {
-		pattern = "*",
-		callback = function()
-			config.apply_highlights()
-		end,
-	})
 end
 
 return M
